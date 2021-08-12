f0c7cfe6bf9d2414123e276a3ac012c1b4f33a7b
Kevin0x000 <jiming.li01@gmail.com>
2021-08-12 13:22:38 +0930

    setup admin logout

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -1,26 +1,50 @@
 import React from 'react'
 import { withRouter} from "react-router-dom"
-import { Layout, Menu} from 'antd';
-
+import { Layout, Menu, Dropdown, message} from 'antd';
+import {CaretDownOutlined, UserOutlined} from '@ant-design/icons'
+import { clearToken } from '../../utils/auth';
 import shopLogo from './shop.png'
 import {adminRoutes} from '../../routes'
+import './frame.css'
+
 
 const { Header, Content, Sider } = Layout;
 
 const routes = adminRoutes.filter(route=>route.isShow)
 
 function index(props) {
+  
+    const menu = (
+    <Menu onClick={p=>{
+      if(p.key === 'logout'){
+        clearToken()
+        props.history.push('/login')
+      }else{
+        message.info(p.key)
+      }
+    }}>
+      <Menu.Item key="notify">Noification Center</Menu.Item>
+      <Menu.Item key="setting">Setting</Menu.Item>
+      <Menu.Item key="logout">Exit</Menu.Item>
+    </Menu>)
     return (
         <Layout>
           <Header className="header">
             <div className="logo">
                 <img src={shopLogo} alt=""/>
             </div>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
               <Menu.Item key="1">nav 1</Menu.Item>
               <Menu.Item key="2">nav 2</Menu.Item>
               <Menu.Item key="3">nav 3</Menu.Item>
-            </Menu>
+            </Menu> */}
+            <Dropdown overlay={menu}>
+              <div className="adminspan">
+                <UserOutlined />&nbsp;
+                <span >Administrator</span>&nbsp;
+                <CaretDownOutlined />
+              </div>
+            </Dropdown>
           </Header>
         <Layout>
           <Sider width={200} className="site-layout-background">
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,6 +5,9 @@ export function getToken(){
 export function setToken(token){
     localStorage.setItem('token', token)
 }
+export function clearToken(){
+    localStorage.removeItem('token')
+}
 
 export function isLogined() {
     if(localStorage.getItem("token")){
